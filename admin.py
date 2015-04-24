2e76610f849b90fc9b81d85c4c39d01d3f853402
poa-webaspect <poa.webaspect@mail.ru>
2015-04-24 09:14:17 +0600

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 
 from .models import *
 
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('personal_number',)
+       
+admin.site.register(Company, CompanyAdmin)
+
 class VacancyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'get_personal_number')
+    list_display = ('owner', 'get_personal_number')
     search_fields = ('id',)
     def get_search_results(self, request, queryset, search_term):
        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
        queryset |= self.model.objects.filter_by_personal_number(search_term)
        return queryset, use_distinct
-    
-       
+   
 admin.site.register(Vacancy, VacancyAdmin)
